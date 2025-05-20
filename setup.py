b18dc39e285ba70c5f599a818794be4160006720
gallg <giuseppe.gallitto01@gmail.com>
2025-05-20 08:56:14 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=["joblib==1.2.0", "matplotlib==3.6.0", "numpy==1.24.2", "pandas==1.5.1", "pygam==0.9.0",
-                      "regressors==0.0.3", "scikit_learn==1.2.1", "scipy==1.11.3", "tqdm==4.64.1", "setuptools==57.5.0"],
+                      "regressors==0.0.3", "scikit_learn==1.2.1", "scipy==1.11.3", "tqdm==4.64.1", "setuptools==80.7.1],
     keywords=['python', 'machine learning', 'neuroimaging', 'data splitting'],
     classifiers=[]
 )
